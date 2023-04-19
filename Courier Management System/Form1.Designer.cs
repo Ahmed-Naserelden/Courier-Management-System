ef1a7223473f3fe6d6b8b1f56fcdaddfa349166c
Ahmed Nasreldeen Amer <an2071497@gmail.com>
2023-04-19 15:44:38 +0200

    Add project files.

---
+++ Courier Management System/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Courier_Management_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.email = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loginbtn
+            // 
+            this.loginbtn.Location = new System.Drawing.Point(268, 323);
+            this.loginbtn.Name = "loginbtn";
+            this.loginbtn.Size = new System.Drawing.Size(294, 47);
+            this.loginbtn.TabIndex = 0;
+            this.loginbtn.Text = "Login";
+            this.loginbtn.UseVisualStyleBackColor = true;
+            this.loginbtn.Click += new System.EventHandler(this.loginbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(132, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "E-mail";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(132, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(238, 163);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(353, 22);
+            this.email.TabIndex = 3;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(238, 240);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(353, 22);
+            this.password.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(275, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(287, 91);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "LOGIN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loginbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Courier Management System/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+namespace Courier_Management_System
+{
+
+
+    
+    public partial class Form1 : Form
+    {
+        OracleConnection conn;
+        string ordb = "Data Source=ORCL;User Id=scott;Password=tiger;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new OracleConnection(ordb);
+            conn.Open();
+        }
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            conn.Dispose();
+        }
+
+        private void loginbtn_Click(object sender, EventArgs e)
+        {
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select * from customers where c_email=:emial and c_password=:password";
+            cmd.CommandType = CommandType.Text;
+
+            string emailtext = email.Text, passwordtext = password.Text;
+            cmd.Parameters.Add("email", emailtext);
+            cmd.Parameters.Add("password", passwordtext);
+
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            string name = "", phone = "", creditCard = "", address = "";
+
+            bool exist = false;
+
+            if (dr.Read())
+            {
+                name = dr[0].ToString();
+                creditCard = dr[1].ToString();
+                phone = dr[4].ToString();
+                address = dr[5].ToString();
+                exist = true;
+            }
+
+            if (exist == true)
+            {
+                MessageBox.Show("Login Succesfully !!");
+            }
+            else
+            {
+                MessageBox.Show("Login faild !! ");
+
+            }
+            //dr.Close();
+        }
+    }
+}
